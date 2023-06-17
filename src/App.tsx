490e4d23782c39e298bce0a46075bea60fa3ff4f
marstarek <tarekahmed1568@gmail.com>
2023-06-17 23:03:38 +0300

    start

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import './App.css'
+import Register from './Register'
+
+function App() {
+
+  return (
+    <main className='App text-3xl'>
+  <Register/>
+    </main>
+  )
+}
+
+export default App
